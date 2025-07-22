b52643beba5ad8eba8636d2e93ed4cc15f0ba720
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-07-21 21:37:30 -0300

    Implementando  a trait dancar para pessoa

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::format;
+
 struct Pessoa {
     name: String,
     age: u8
@@ -13,10 +15,21 @@ impl Pessoa{
     }
 }
 
+trait Dancar {
+    fn dancar(&self) -> String;
+}
+
+impl Dancar for Pessoa {
+    fn dancar(&self) -> String {
+        return format!("{} esta dancando", self.name);
+    }
+}
+
 
 fn main(){
 
 
     let pessoa = Pessoa::new("Daniel".to_string(), 23);
     pessoa.to_string();
+    print!("{}",pessoa.dancar());
 }
