6dbcec23ef1afef3c8f67266fc56b19057d93a31
daniel <daniel.cordeiro2@fatec.sp.gov.br>
2025-08-20 22:13:46 -0300

    Criando traits ambiente bancario

--- src/structs/banco_structs.rs
+++ src/structs/banco_structs.rs
@@ -1,4 +1,3 @@
-
 pub struct ContaSimples {
     pub titular: String,
     pub saldo: f64,
@@ -18,4 +17,25 @@ pub struct ContaInvestimento {
     pub carteira: Vec<String>,
     pub rendimento_anual: f64,
     pub risco: String,
+}
+
+pub trait OperacoesSimples {
+    fn depositar(&mut self, valor: f64);
+    fn sacar(&mut self, valor: f64) -> bool;
+    fn consultar_saldo(&self) -> f64;
+}
+
+pub trait OperacoesCorrente {
+    fn depositar(&mut self, valor: f64);
+    fn sacar(&mut self, valor: f64) -> bool;
+    fn consultar_saldo(&self) -> f64;
+    fn usar_limite(&mut self, valor: f64) -> bool;
+    fn transferir(&mut self, valor: f64, destino: &mut ContaCorrente) -> bool;
+}
+
+pub trait OperacoesInvestimento {
+    fn consultar_saldo(&self) -> f64;
+    fn investir(&mut self, ativo: String, valor: f64) -> bool;
+    fn calcular_rendimento(&self) -> f64;
+    fn avaliar_risco(&self) -> &str;
 }
